1b188c94d07b8c2974d2d3c020088764096e1cdd
Medi W. Assumani <mediassumani49@gmail.com>
2017-11-30 22:09:15 -0500

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,3 +1,15 @@
 #include "source.h"
 using namespace std;
 
+using namespace std;
+
+//implementation of the list class/ADT
+
+ListType::ListType(int array[], int length){
+  
+  //initializing the members
+  this->list[]= array[];
+  this->legth[]= length;
+  
+}
+
