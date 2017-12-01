b9e684a23cc661a32d073b96d6f139bdbaef0271
Medi W. Assumani <mediassumani49@gmail.com>
2017-11-30 22:33:13 -0500

    Update source.h
    commenting the functions

--- source.h
+++ source.h
@@ -7,13 +7,13 @@ class ListType
 
     public:
             //public members
-        bool isEmpty()const;
-        bool isFull()const;
-        int searchList(int item)const;
-        void insertElement(int new_element);
-        void removeElement(int element);
-        void destroyList();
-        void printList();
+        bool isEmpty()const;// this function checks if the list is empty
+        bool isFull()const;// this function checks if the list is full
+        int searchList(int item)const;// this function searches the list for a specific element (integer)
+        void insertElement(int new_element);// this function adds an element in the list
+        void removeElement(int element);// this function removes an lement in the list
+        void destroyList();// this elemnts eradicates the entire list and its elements
+        void printList();// this function prints the entire list
         ListType();//constructor
 
     private: //private members
