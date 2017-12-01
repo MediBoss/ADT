daf6a669557376bdd2a6f524ef23d83d4314d310
Medi W. Assumani <mediassumani49@gmail.com>
2017-11-30 22:30:06 -0500

    Update list.cpp
    implementing the isEmpty() function

--- list.cpp
+++ list.cpp
@@ -13,3 +13,17 @@ ListType::ListType(int array[], int length){
   
 }
 
+bool ListType:: isEmpty(){
+  
+ for(int i = 0; i < this->length; i++){
+    //if there's at least one lement in the list
+        if(this.>list[i] != NULL){
+   //then the list is not empty
+          return false; 
+        }else{
+         continue;// increment the indexand go to the elemnet 
+        }
+   }
+  return true; //returns true if each index of the array does not contain an element or elemnt is zero
+}
+
