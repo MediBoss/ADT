5b0a2b10f8c31afd80f7ffdb91a3f2f75185aaa7
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-01 23:45:02 -0500

    Update source.h
    adding the declaration of sortList() and swap() function

--- source.h
+++ source.h
@@ -10,6 +10,8 @@ class ListType
         bool isEmpty()const;// this function checks if the list is empty
         bool isFull()const;// this function checks if the list is full
         int searchList(int item)const;// this function searches the list for a specific element (integer)
+        static inline void swap(int& a, int& b); // this is an helper function to the sort function to swap two values
+        void sortList(); // this function will implement the selction sort to arrange the array
         void insertElement(int new_element);// this function adds an element in the list
         void removeElement(int element);// this function removes an lement in the list
         void destroyList();// this elemnts eradicates the entire list and its elements
