29dfed091f30e2aaae058f459b3a7edf0a9d6c18
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-01 23:14:22 -0500

    Update list.cpp
    updating the constructor by adding parameter reference on the array.

--- list.cpp
+++ list.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 //implementation of the list class/ADT
 
-ListType::ListType(int array[], int length){
+//@param array[] is the array to be passed and @param length is the length of the array
+ListType::ListType(int& array[], int length){
   
   //initializing the members
   this->list[]= array[];
