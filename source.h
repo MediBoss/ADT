0dc87f872a06af48aaacc92e341a8610efec0916
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-03 17:14:08 -0500

    Update source.h

--- source.h
+++ source.h
@@ -5,8 +5,14 @@ class ListType
 
     public:
             //public members
-        bool isEmpty()const;// this function checks if the list is empty
-        bool isFull()const;// this function checks if the list is full
+        bool isEmpty()const;
+         // this function checks if the list is empty
+         //postcondition : true is returned if the array is empty and false is returned if the array isn't
+  
+        bool isFull()const;
+        // this function checks if the list is full
+        //postcondition : true is returned if the array is full and false is returned if the array isn't
+  
         int searchList(int item)const;// this function searches the list for a specific element (integer)
         static inline void swap(int& a, int& b); // this is an helper function to the sort function to swap two values
         void sortList(); // this function will implement the selction sort to arrange the array
